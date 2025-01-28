5b61d606c61bd09b5e9826788c8f003f55c8cc2a
Edwin Yoner <edwinyoner@gmail.com>
2025-01-28 07:03:14 -0500

    Clase 1

--- BLOQUE_I/CLASE_04/programa_01.py
+++ BLOQUE_I/CLASE_04/programa_01.py
@@ -4,4 +4,3 @@ print(objetos["oficina"][0])
 print(objetos["cocina"][1])
 objetos.clear()
 print(objetos)
-#https://www.xnxx.es/video-17qc3x4b/follando_a_escondidas_con_mi_novio_me_encanta_la_adrenalina_y_su_leche_adentro
