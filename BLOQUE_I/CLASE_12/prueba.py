8011cb47a1dc6c6f04d694257d21635874beb4cb
Edwin Yoner <edwinyoner@gmail.com>
2025-02-13 07:38:15 -0500

    Clase 13

--- BLOQUE_I/CLASE_12/prueba.py
+++
@@ -1,8 +0,0 @@
-def suma_recursiva(lista):
-    if not lista:
-        return 0
-    return lista[0] + suma_recursiva(lista[1:])
-
-s = [2, 3, 4, 1, 0, 5]
-resultado = suma_recursiva(s)
-print(f"La suma de los elementos de la lista es: {resultado}")
